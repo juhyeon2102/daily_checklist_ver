d21b69dea26164bb90bccfa3a551efd3f88ad47c
haleyhyeinpark <hyein9653@naver.com>
2020-12-03 16:46:03 +0900

    firstCommend

---
+++ app/src/main/java/com/haley/a1stcl/MainActivity.java
@@ -0,0 +1,24 @@
+package com.haley.a1stcl;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.start);
+        start.setOnClickListener(view -> {
+            Intent in = new Intent(MainActivity.this, list.class);
+            startActivity(in);
+        });
+    }
+
+}
---
+++ app/src/main/java/com/haley/a1stcl/list.java
@@ -0,0 +1,22 @@
+package com.haley.a1stcl;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+
+public class list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        toolBarLayout.setTitle("오늘의 CheckList");
+
+    }
+}
